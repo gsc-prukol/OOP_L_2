703e92e013a793c627871ac1d87532b6c55f5460
gsc-prukol <gsc.prukol@gmail.com>
2018-04-16 21:59:09 +0300

    Елемент черги готовий.

--- OOP_L_2/Sourse.cpp
+++ OOP_L_2/Sourse.cpp
@@ -2,11 +2,23 @@
 PartyQueue::PartyQueue()
 {
 	surname = "Surname";
-	phone = "+1202 - 456 - 1111";
+	phone = "+1 202 - 456 - 1111";
 }
 PartyQueue::PartyQueue(string & Surname, string & Phone, Date & Date)
 {
 	surname = Surname;
 	phone = Phone;
 	date = Date;
+}
+PartyQueue::PartyQueue(const PartyQueue &  sourse)
+{
+	surname = sourse.surname;
+	phone = sourse.phone;
+	date = sourse.date;
+}
+PartyQueue::~PartyQueue()
+{
+	surname.~string();
+	phone.~string();
+	date.~Date();
 }
